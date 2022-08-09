9e5d9a55b18e0d2f439f3387ec99805bd4f57ee7
Sonia Rosales <103076929+srrosales20@users.noreply.github.com>
2022-08-09 16:15:44 +0000

    changed the route

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-var http = require("http");
+//rerouting 
+var http = require("http"); 
 http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
+  var path = req.url.toLowerCase(); //makes lowercase
+  switch(path) {
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Home page'); //return homepage
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('About page'); //returns abt page
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');  //returns nor found
+      break;
+    }
 }).listen(process.env.PORT || 3000);
+
+
+
+
+// var http = require("http");
+// http.createServer(function(req,res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Aloha world');
+// }).listen(process.env.PORT || 3000);
